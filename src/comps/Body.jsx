bc8a42e1d2e7b5f5f26efb05d07e3329b863cd1f
Emmanuel Korir <korirm3@gmail.com>
2021-09-07 00:32:04 +0300

    Added Fix

--- src/comps/Body.jsx
+++ src/comps/Body.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
-import Logo from '../assets/logo.svg'
 import J from '../assets/J.png'
+import J2 from '../assets/J2.png'
+import J4 from '../assets/J4.png'
+import room from '../assets/room.png'
+
 
 const Body = () => {
+    const style = {
+        backgroundImage:`url(${room})`,
+    }
+    
     return (
        <div className="j_section">
            <div className="j_section_wrapper_1">
-               <img src={J} alt="" className="j_section_wrapper_1_img" />
+               <span className="j_section_wrapper_1_span_A">A</span>
+               <img src={J4} alt="" className="j_section_wrapper_1_img" />
+               <span className="j_section_wrapper_1_span_I">I</span>
+
            </div>
            <div className="j_section_wrapper">
                <img src={J} alt="" className="j_section_wrapper_img" />
+               <span className="j_section_wrapper_1_span_R">R</span>
            </div>
-
+        <div className="j_section_end" style={style}>
+            {/* <img src={room} alt="" className="j_section_img" /> */}
+        </div>
        </div>
     )
 }
